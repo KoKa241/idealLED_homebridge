91aaec59e458c10a5dd44cdf7cd25344b30d4040
KoKa241 <vlad74985@gmail.com>
2026-07-16 19:17:01 +0200

    fix: correctly map PLUGIN_NAME for Homebridge 2.x

--- dist/index.js
+++ dist/index.js
@@ -2,6 +2,6 @@
 const settings_1 = require("./settings");
 const platform_1 = require("./platform");
 module.exports = (api) => {
-    api.registerPlatform(settings_1.PLATFORM_NAME, platform_1.IdealLEDPlatform);
+    api.registerPlatform(settings_1.PLUGIN_NAME, settings_1.PLATFORM_NAME, platform_1.IdealLEDPlatform);
 };
 //# sourceMappingURL=index.js.map
